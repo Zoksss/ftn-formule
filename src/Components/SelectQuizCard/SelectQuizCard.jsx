1286f416e991dcac4243f17acac735a9d25ba247
Zoksss <zorantodorovic80@gmail.com>
2022-06-17 23:29:03 +0200

    more questions

--- src/Components/SelectQuizCard/SelectQuizCard.jsx
+++ src/Components/SelectQuizCard/SelectQuizCard.jsx
@@ -11,12 +11,13 @@ const SelectQuizCard = (props) => {
                 <p className="question-count-num">14</p>
             </div>
             <div className="quiz-select-button-container">
-               <Quiz number="0" title="Sve oblasti" questionNumber="6" startQuiz = {props.startQuiz}/>
-               <Quiz number="1" title="Algebra" questionNumber="0"/>
-               <Quiz number="2" title="Jednacine" questionNumber="0"/>
-               <Quiz number="3" title="Eksponencijalne i logaritmi" questionNumber="0"/>
-               <Quiz number="4" title="Triganometrija" questionNumber="0"/>
-               <Quiz number="5" title="Vektori" questionNumber="6" startQuiz = {props.startQuiz}/>
+                <Quiz number="0" title="Sve oblasti" questionNumber="11" startQuiz={props.startQuiz} />
+                <Quiz number="1" title="Algebra" questionNumber="0" />
+                <Quiz number="2" title="Jednacine" questionNumber="0" />
+                <Quiz number="3" title="Eksponencijalne i logaritmi" questionNumber="0" />
+                <Quiz number="4" title="Triganometrija" questionNumber="0" />
+                <Quiz number="5" title="Vektori" questionNumber="6" startQuiz={props.startQuiz} />
+                <Quiz number="6" title="Analitcka geometrija" questionNumber="5" startQuiz={props.startQuiz} />
             </div>
         </div>
     )
--- src/questions.jsx
+++ src/questions.jsx
@@ -41,6 +41,48 @@ const questions = [
         hasImage: false,
         imagePath: "/",
     },
+    {
+        category: "Vektori",
+        question: "Tacka S na sredini 2 tacke se racuna:",
+        correctAnswer: "(x1+x2/2,y1+x2/2)",
+        hasImage: false,
+        imagePath: "/",
+    },
+    {
+        category: "Analitcka geometrija",
+        question: "Opsta formula prave",
+        correctAnswer: "y=kx+n",
+        hasImage: false,
+        imagePath: "/",
+    },
+    {
+        category: "Analitcka geometrija",
+        question: "Prava koja prolazi kroz tacku i imamo k:",
+        correctAnswer: "y-y1=k(x-x1)",
+        hasImage: false,
+        imagePath: "/",
+    },
+    {
+        category: "Analitcka geometrija",
+        question: "Prava koja prolazi kroz 2 tacke:",
+        correctAnswer: "y-y1=y2-y1/x2-x1(x-x1)",
+        hasImage: false,
+        imagePath: "/",
+    },
+    {
+        category: "Analitcka geometrija",
+        question: "k2 kada je prava normalna:",
+        correctAnswer: "-1/k1",
+        hasImage: false,
+        imagePath: "/",
+    },
+    {
+        category: "Analitcka geometrija",
+        question: "tgL izmedju prava gde imamo k",
+        correctAnswer: "k2-k1/1+k1*k2",
+        hasImage: false,
+        imagePath: "/",
+    },
     
     
 ];
